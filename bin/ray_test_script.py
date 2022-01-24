ab9eb50e968dfb2f659367886e00ccbdba388214
Markus Kaiser <markus.kaiser@siemens.com>
2022-01-24 15:40:12 +0000

    Fix style issues with the test script

--- bin/ray_test_script.py
+++ bin/ray_test_script.py
@@ -7,25 +7,27 @@ import ray
 ray.init(address="auto")
 
 print(
-    """This cluster consists of
-    {} nodes in total
-    {} CPU resources in total
-""".format(
-        len(ray.nodes()), ray.cluster_resources()["CPU"]
-    )
+    f"""This cluster consists of
+    {len(ray.nodes())} nodes in total
+    {ray.cluster_resources()["CPU"]} CPU resources in total
+"""
 )
 
 
 @ray.remote
-def f():
+def echo_local_ip() -> str:
+    """Wait for a bit and return the local IP address.
+
+    Returns:
+        str: The local IP address
+    """
     time.sleep(0.001)
-    # Return IP address.
     return socket.gethostbyname(socket.gethostname())
 
 
-object_ids = [f.remote() for _ in range(10000)]
+object_ids = [echo_local_ip.remote() for _ in range(10000)]
 ip_addresses = ray.get(object_ids)
 
 print("Tasks executed")
 for ip_address, num_tasks in Counter(ip_addresses).items():
-    print("    {} tasks on {}".format(num_tasks, ip_address))
+    print(f"    {num_tasks} tasks on {ip_address}")
